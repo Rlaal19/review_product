02ced8454533223b6d2f6e28d6ca1ade741d345a
Rlaal19 <n.prarichaya23@gmail.com>
2024-04-19 16:08:15 +0700

    hash password

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,7 @@
 const express = require("express")
-const mongoose  =require('mongoose')
+const mongoose = require('mongoose')
 const cors = require('cors')
+const bcrypt = require('bcrypt')
 const RegisterModel = require('./models/Employee')
 const EmployeeModel = require("./models/Employee")
 
@@ -10,29 +11,36 @@ app.use(cors())
 
 mongoose.connect("mongodb+srv://admin:2319@cluster0.kdh9hcl.mongodb.net/review-product");
 
-app.post('/login', (req, res) =>{
-    const{email,password} = req.body;
-    EmployeeModel.findOne({email,password})
-    .then(user =>{
-        if(user){
-            if(user.password === password){
-                res.json("Success")
-            }else{
-                res.json("Pass word is not correct")
-            }
-        }else{
+app.post('/login', (req, res) => {
+    const { email, password } = req.body;
+    EmployeeModel.findOne({ email: email})
+    .then(user => {
+        if (user) {
+            bcrypt.compare(password, user.password, (err, response) => {
+                if (response) {
+                    res.json("Success")
+                } else{
+                    res.json("Password is not correct")
+                }
+            })
+            
+        } else {
             res.json("No record exited")
         }
     })
 })
 
-app.post('/register', (req, res) =>{
-    EmployeeModel.create(req.body)
-    .then(employees => res.json(employees))
-    .catch(err => res.json(err))
+app.post('/register', (req, res) => {
+    const { name, email, password } = req.body;
+    bcrypt.hash(password, 10)
+        .then(hash => {
+            EmployeeModel.create({ name, email, password: hash })
+                .then(employees => res.json(employees))
+                .catch(err => res.json(err))
+        }).catch(err => console.log(err.message))
 
 })
 
-app.listen(3001, () =>{
+app.listen(3001, () => {
     console.log("server is running")
 })
